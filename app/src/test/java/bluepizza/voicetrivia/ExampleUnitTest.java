662ad241501f14b0ed46db5abf23e47d9268cc56
jin <JSBx86@gmail.com>
2016-08-11 19:48:44 -0400

    I actually got this working!

--- app/src/test/java/bluepizza/voicetrivia/ExampleUnitTest.java
+++ app/src/test/java/bluepizza/voicetrivia/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package bluepizza.voicetrivia;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -10,8 +10,8 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
 
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() throws Exception
+    {
         assertEquals(4, 2 + 2);
     }
-
 }
